9efd18c11855a298d9835cd4b340ef8682c2700e
jbasualdo <jbasualdo@cegsoft.com>
2023-09-03 23:46:33 -0300

    feature(Background) : Create Background component

--- src/components/Background.js
+++ src/components/Background.js
@@ -12,7 +12,6 @@ export const Background = ({ children }) => {
 
 const BG = styled.div`
     height: 100vh;
-    /* background-image: url('https://images.ctfassets.net/s5n2t79q9icq/2Eq0mcvZ8Pw9H57HaVRZrq/6af5fd80dcea300cb818e1b014c98dc1/v4anjdKFGB_1600x1080-80.jpg?q=70'); */
-    /* background-repeat: no-repeat; */
-    /* background-position: center; */
+    background-repeat: no-repeat;
+    background-position: center;
 `
