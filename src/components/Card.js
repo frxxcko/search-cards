81dd13defbfe5ba59d63803e95ea46e132b707aa
jbasualdo <jbasualdo@cegsoft.com>
2023-09-03 23:47:35 -0300

    style(Card) : set inline style logic to set background image comming from API

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const Card = ({ className, children, addDividers, showCloseButton, style, close }) => {
+export const Card = ({ className, children, addDividers, showCloseButton, style, close, cardImageURL }) => {
     const isRoutesType = (child) => typeof child === 'object' && child?.type?.name === 'Routes';
 
     return (
-        <div style={style} className={["card border-0 rounded-1", className ? className : ""].join(" ").trim().replace(/\s+/g, " ")} >
+        <div style={{ backgroundImage: `url(${cardImageURL})`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', ...style }} className={["card border-0 rounded-1 card-background", className ? className : ""].join(" ").trim().replace(/\s+/g, " ")} >
             {showCloseButton && (
                 <div className='d-flex justify-content-end'>
                     <i className="fas fa-times et-coupon-close-btn" onClick={close}></i>
