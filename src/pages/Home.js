99067b1bac7af8dab899e7716fe0c8bcf3c166fc
jbasualdo <jbasualdo@cegsoft.com>
2023-09-03 23:49:22 -0300

    feature(Home) : API calls, implementation of Zustand, conditional rendering, deletion of CardItems

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,12 +5,24 @@ import { Button } from '../components/Button';
 import { Card } from '../components/Card';
 import { AxiosInstance } from '../services/AxiosService';
 
+import { useStore } from '../zustand/Store';
+
+import { LocalStorageService } from '../services/LocalStorageService';
+import { CardItem } from '../components/CardItem';
+
+
 export const Home = () => {
-    const [cardName, setCardName] = useState('Underworld Dreams');
+    const [cardName, setCardName] = useState('');
+    const [card, setCard] = useState(null);
+
+    const [cardList, addCard, deleteCard, clearList] = useStore((state) => [state.cardList, state.addCard, state.deleteCard, state.clearList])
+
     const btnRef = useRef();
+
     const setSC_URL = (cardName) => `https://starcitygames.com/search/?card_name=${cardName}`;
     const setCK_URL = (cardName) => `https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=${cardName}`
-    const [card, setCard] = useState(null);
+
+
 
     const onSearchBtnClick = () => {
         const inputText = btnRef.current.value.trim();
@@ -20,32 +32,60 @@ export const Home = () => {
     }
 
     useEffect(() => {
-        try {
-            AxiosInstance.get(`cards/named?fuzzy=${cardName}`).then(res => {
+        console.log(cardList)
+        AxiosInstance.get(`cards/random`)
+            .then(res => {
+                let card = res.data;
+                if (card !== null || card !== undefined) {
+                    setCard(card)
+                }
                 console.log(res)
             })
-        } catch (error) {
-            console.log(error)
+            .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => (console.log(cardList)), [cardList])
+
+    useEffect(() => {
+        if (cardName) {
+            AxiosInstance.get(`cards/named?fuzzy=${cardName}`)
+                .then(res => {
+                    let card = res.data;
+                    if (card !== null || card !== undefined) {
+                        addCard({ id: card.id, name: card.name, image: card.image_uris.png })
+                        setCard(card)
+                    }
+                    console.log(res)
+                    btnRef.current.value = '';
+                })
+                .catch(err => console.log(err))
+            console.log(cardList)
         }
     }, [cardName]);
 
     return (
         <Background>
-            <div className='d-flex flex-column align-items-center gap-2'>
+            <Card className='d-flex flex-column align-items-center gap-2 px-5 py-5 card-blur' cardImageURL={card?.image_uris?.art_crop}>
+                {card && <img style={{ height: '400px' }} className='mb-1' src={card.image_uris.png} alt={card.name} />}
+                <h3 className='mb-1'>{card?.name ?? cardName}</h3>
                 <div className='d-flex gap-2'>
                     <Input ref={btnRef} setCardName={setCardName} />
-                    <Button text='Buscar' onClick={onSearchBtnClick} />
+                    <Button text='Search' onClick={onSearchBtnClick} />
                 </div>
-                <h3 className='mt-5'>{cardName}</h3>
-                <div className='d-flex gap-5 mt-5'>
+                <div className='my-1 d-flex gap-2 card-list'>
+                    {cardList && cardList.map((card, i) => (
+                        <CardItem card={card} key={`card-${i}`} deleteCard={deleteCard} />
+                    ))}
+                </div>
+                <div className='d-flex gap-5'>
                     <Button>
-                        <a target='_blank' rel='noreferrer' href={setCK_URL(cardName)}>Check '<span style={{ fontWeight: 'bold' }}>{cardName}</span>' in CardKindom</a>
+                        <a target='_blank' rel='noreferrer' href={setCK_URL(card?.name ?? cardName)}>Check '<span style={{ fontWeight: 'bold' }}>{card?.name ?? cardName}</span>' in CardKindom</a>
                     </Button>
                     <Button>
-                        <a target='_blank' rel='noreferrer' href={setSC_URL(cardName)}>Check '<span style={{ fontWeight: 'bold' }}>{cardName}</span>' in StarCity</a>
+                        <a target='_blank' rel='noreferrer' href={setSC_URL(card?.name ?? cardName)}>Check '<span style={{ fontWeight: 'bold' }}>{card?.name ?? cardName}</span>' in StarCity</a>
                     </Button>
                 </div>
-            </div>
+            </Card>
         </Background>
     )
 }
