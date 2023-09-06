1d2b488d580e1c320238bbd325d1b8a20c31d382
jbasualdo <jbasualdo@cegsoft.com>
2023-09-06 19:48:02 -0300

    styles(CardItem) : remove border

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,7 +4,7 @@ export const CardItem = ({ card, deleteCard, selectCard }) => {
     return (
         <div className='d-flex gap-1 card-list-item'>
             <i className="fa-regular fa-circle-xmark" onClick={() => deleteCard(card.id)} style={{ color: '#ffffff' }}></i>
-            <div className='d-flex flex-column align-items-center' onClick={() => selectCard(card.id)} style={{ cursor: 'pointer', border: '1px solid #fff' }}>
+            <div className='d-flex flex-column align-items-center' onClick={() => selectCard(card.id)} style={{ cursor: 'pointer' }}>
                 <img style={{ height: '150px' }} src={card.image} alt={card.name} />
                 <h6>{card.name}</h6>
             </div>
