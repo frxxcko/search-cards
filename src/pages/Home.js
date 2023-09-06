1516986873003dcdb545346691c658fa370b8cd8
jbasualdo <jbasualdo@cegsoft.com>
2023-09-06 19:44:15 -0300

    Use of new store methods

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import { Button } from '../components/Button';
 import { Card } from '../components/Card';
 import { AxiosInstance } from '../services/AxiosService';
 
+import { mapCard } from '../helpers/MapCard';
 import { useStore } from '../zustand/Store';
 
 import { LocalStorageService } from '../services/LocalStorageService';
@@ -13,16 +14,13 @@ import { CardItem } from '../components/CardItem';
 
 export const Home = () => {
     const [cardName, setCardName] = useState('');
-    const [card, setCard] = useState(null);
 
-    const [cardList, addCard, deleteCard, clearList] = useStore((state) => [state.cardList, state.addCard, state.deleteCard, state.clearList])
+    const [card, cardList, setCard, addCard, deleteCard, selectCard, clearList] = useStore((state) => [state.card, state.cardList, state.setCard, state.addCard, state.deleteCard, state.selectCard, state.clearList])
 
     const btnRef = useRef();
 
     const setSC_URL = (cardName) => `https://starcitygames.com/search/?card_name=${cardName}`;
-    const setCK_URL = (cardName) => `https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=${cardName}`
-
-
+    const setCK_URL = (cardName) => `https://www.cardkingdom.com/catalog/search?search=header&filter%5Bname%5D=${cardName}`;
 
     const onSearchBtnClick = () => {
         const inputText = btnRef.current.value.trim();
@@ -37,7 +35,7 @@ export const Home = () => {
             .then(res => {
                 let card = res.data;
                 if (card !== null || card !== undefined) {
-                    setCard(card)
+                    setCard(mapCard(card))
                 }
                 console.log(res)
             })
@@ -52,8 +50,8 @@ export const Home = () => {
                 .then(res => {
                     let card = res.data;
                     if (card !== null || card !== undefined) {
-                        addCard({ id: card.id, name: card.name, image: card.image_uris.png })
-                        setCard(card)
+                        addCard(mapCard(card))
+                        setCard(mapCard(card))
                     }
                     console.log(res)
                     btnRef.current.value = '';
@@ -66,7 +64,7 @@ export const Home = () => {
     return (
         <Background>
             <Card className='d-flex flex-column align-items-center gap-2 px-5 py-5 card-blur' cardImageURL={card?.image_uris?.art_crop}>
-                {card && <img style={{ height: '400px' }} className='mb-1' src={card.image_uris.png} alt={card.name} />}
+                {card && <img style={{ height: '400px' }} className='mb-1' src={card.image} alt={card.name} />}
                 <h3 className='mb-1'>{card?.name ?? cardName}</h3>
                 <div className='d-flex gap-2'>
                     <Input ref={btnRef} setCardName={setCardName} />
@@ -74,7 +72,7 @@ export const Home = () => {
                 </div>
                 <div className='my-1 d-flex gap-2 card-list'>
                     {cardList && cardList.map((card, i) => (
-                        <CardItem card={card} key={`card-${i}`} deleteCard={deleteCard} />
+                        <CardItem card={card} key={`card-${i}`} deleteCard={deleteCard} selectCard={selectCard} />
                     ))}
                 </div>
                 <div className='d-flex gap-5'>
