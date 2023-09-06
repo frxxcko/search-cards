842095576198f18f57171d23b2baf85d6faa5482
jbasualdo <jbasualdo@cegsoft.com>
2023-09-06 19:44:31 -0300

    New implementations to store and card caching

--- src/zustand/Store.js
+++ src/zustand/Store.js
@@ -1,8 +1,11 @@
 import { create } from 'zustand'
 
 export const useStore = create((set) => ({
+    card: null,
     cardList: [],
+    setCard: (card) => set(state => state.card = card),
     addCard: (card) => set(state => ({ cardList: [...state.cardList, card] })),
     deleteCard: (cardId) => set(state => ({ cardList: state.cardList.filter(card => card.id !== cardId) })),
+    selectCard: (cardId) => set(state => ({ card: state.cardList.find(card => card.id === cardId) })),
     clearList: () => set(() => ({ cardList: [] }))
 }))
